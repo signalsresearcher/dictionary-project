43c9d00d5e16df5a3e662d43ad8f01f68434910e
signalsresearcher <74799594+signalsresearcher@users.noreply.github.com>
2022-02-11 17:08:28 -0500

    display word definitions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,32 +1,42 @@
 import React, { useState } from 'react';
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
         let [keyword, setKeyword] = useState("");
+        let [results, setResults] = useState(null);
 
-function handleResponse(response) {
-
-}
+        function handleResponse(response) {
+         setResults(response.data[0]);
+    }
 
         function search(event) {
             event.preventDefault();
-        alert(`searching for the definition of ${keyword}`);
+            
+            let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+            axios.get(apiUrl).then(handleResponse);
+           
     }
 
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
     
-   function handleKeywordChange (event) {
+
+   function handleKeywordChange(event) {
        setKeyword(event.target.value);
    }
 
+    
 
-    return (
+  return (
         <div className="Dictionary">
             <form onSubmit={search}>
-                <input type="search" onChange={handleKeywordChange} autoFocus={true} />
+                <input 
+                type="search" 
+                onChange={handleKeywordChange} 
+                autoFocus={true} />
             </form>
+
+            <Results results={results} />
         </div>
-    ) 
-}
+    );
+}  
