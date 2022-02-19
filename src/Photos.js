887311f5d0ccd020aa906c33057f4357fa238f91
signalsresearcher <74799594+signalsresearcher@users.noreply.github.com>
2022-02-19 06:40:28 -0500

    Added.Photos.js

---
+++ src/Photos.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Photos(props) {
+    if (props.photos) {
+    return (
+        <div className="Photos">
+            {props.photos.map(function (photo, index) {
+                return ( <img src={photo.src.tiny} key={index}/>
+                );
+                })}
+        </div>
+    );
+    } else {
+        return null;
+    }
+}
