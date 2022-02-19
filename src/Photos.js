5c8590b67630906b5e8230372cec6688c1f8f666
signalsresearcher <74799594+signalsresearcher@users.noreply.github.com>
2022-02-19 07:48:57 -0500

    added and styled images

--- src/Photos.js
+++ src/Photos.js
@@ -1,14 +1,18 @@
 import React from "react";
+import "./Photos.css";
 
 export default function Photos(props) {
     if (props.photos) {
     return (
-        <div className="Photos">
+        <section className="Photos">
+            <div className="row-6"> 
             {props.photos.map(function (photo, index) {
+            
                 return ( <img src={photo.src.tiny} key={index} alt="image"/>
                 );
                 })}
-        </div>
+            </div>
+        </section>
     );
     } else {
         return null;
